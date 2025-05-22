7adb7d6ee2654f4f2f9bf6757285b634237fd19a
Fath <miloszkielt@gmail.com>
2025-05-22 20:15:23 +0200

    Add first draft of backend endpoints

---
+++ backend/backend.py
@@ -0,0 +1,44 @@
+from fastapi import UploadFile, HTTPException, FastAPI
+from core.file_ingestor import FileIngestor
+from config import Config
+from pydantic import BaseModel
+import os
+
+app = FastAPI()
+ingestor = FileIngestor()
+
+
+@app.post("/upload")
+async def upload(file: UploadFile):
+    file_path = f"{Config.UPLOAD_DIR}/{file.filename}"
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+    ingestor.ingest(file_path)
+    return {"status": "success", "file_path" : file_path}
+
+
+class QueryMessage(BaseModel):
+    query: str
+
+@app.post("/query")
+async def query(query: QueryMessage):
+    try:
+        if not os.path.exists(Config.VECTOR_DB_DIR):
+            raise HTTPException(
+                status_code=404,
+                detail="No documents found. Upload files first."
+            )
+        
+        # Retrieve relevant chunks
+        docs = rag_engine.retrieve(query.query)
+        
+        # Format response
+        return {
+            "query": query,
+            "results": [{"content": doc.page_content} for doc in docs]
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Retrieval failed: {str(e)}"
+        )
